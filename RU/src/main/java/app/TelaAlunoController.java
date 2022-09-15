f026f4bb3e09382c9e288483779cc3a8b7bcfb90
daniels6738 <danieleao@live.com>
2022-09-15 11:40:06 -0300

    Implementando navegação ir e voltar para login, tela principal e tela compra

--- RU/src/main/java/app/TelaAlunoController.java
+++ RU/src/main/java/app/TelaAlunoController.java
@@ -1,13 +1,25 @@
 package app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import negocio.Controlador;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class TelaAlunoController {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private Label labelBemvindo;
@@ -18,10 +30,22 @@ public class TelaAlunoController {
     }
 
     @FXML
-    protected void botaoComprarApertar(){}
+    protected void botaoComprarApertar(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TelaCompra.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
-    protected void botaoSair(){}
+    protected void botaoSair(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     protected void botaoVerCardapioApertar(){}
